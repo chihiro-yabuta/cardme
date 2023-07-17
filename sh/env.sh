c86898117ab9fcc4e7ae1b9a1ef3cf5d8be3370d
chihiro-yabuta <ja.chihiro.yabuta@gmail.com>
2023-07-17 15:55:38 +0000

    add escape func about aws conf

--- sh/env.sh
+++ sh/env.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# create policies https://eksctl.io/usage/minimum-iam-policies/
-# create user
-# create Users > user > Access keys
-# download csv
-
 email=`git config user.email`
 name=`git config user.name`
 
@@ -14,5 +9,15 @@ else
   echo "git_email=${email}\ngit_name=${name}" > .env
 fi
 
-IFS=, read -ra parts <<< "$(sed -n '2p' accessKeys.csv)"
-echo "access=${parts[0]}\nsecret=${parts[1]}" >> .env
+if [ -e "accessKeys.csv" ]; then
+  IFS=, read -ra parts <<< "$(sed -n '2p' accessKeys.csv)"
+  echo cmd1=aws configure set aws_access_key_id ${parts[0]} >> .env
+  echo cmd2=aws configure set aws_secret_access_key ${parts[1]} >> .env
+  echo cmd3=aws configure set region ap-northeast-1 >> .env
+  echo cmd4=eksctl utils write-kubeconfig --cluster=cardme-cluster >> .env
+else
+  echo cmd1=echo "no aws config" >> .env
+  echo cmd2=echo "no aws config" >> .env
+  echo cmd3=echo "no aws config" >> .env
+  echo cmd4=echo "no aws config" >> .env
+fi
