05a12c84bed1c3f0a6f12b91715232edd095e8a5
chihiro.a.yabuta <chihiro.a.yabuta@rakuten.com>
2024-05-14 02:39:17 +0000

    update dockerfile

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 email=`git config user.email`
 name=`git config user.name`
 
-if [ "$1" == "master" ]; then
+if [ "$1" == "admin" ]; then
   echo "git_email=ja.chihiro.yabuta@gmail.com\ngit_name=chihiro-yabuta" > .env
 else
   echo "git_email=${email}\ngit_name=${name}" > .env
